18b9291a6cc3e1da352635a71a67e953663fbbdf
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-16 10:44:42 -0700

    Add main exports

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.generatePresets = require('./lib/presets')
+module.exports.generateTranslations = require('./lib/translations')
+module.exports.generateTranslate = require('./lib/translate')
