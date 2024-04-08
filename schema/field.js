5d75ebb472e188f835732b175a3251ff6a778c05
Tomás Ciccola <tciccola@digital-democracy.com>
2024-04-08 14:00:07 -0300

    rebuild lockfile, remove `required` from schema/{field,preset}.js

--- schema/field.js
+++ schema/field.js
@@ -1,5 +1,2 @@
 import { valueSchemas } from '@mapeo/schema'
 export const fieldSchema = valueSchemas['field']
-// mapeo-schema defines presets as having an id property. For preset definitions
-// this comes from the filename, so it is not required in the JSON definitions
-export const required = fieldSchema.required.filter(prop => prop !== 'id')
--- schema/preset.js
+++ schema/preset.js
@@ -1,5 +1,2 @@
 import { valueSchemas } from '@mapeo/schema'
 export const presetSchema = valueSchemas['preset']
-// mapeo-schema defines presets as having an id property. For preset definitions
-// this comes from the filename, so it is not required in the JSON definitions
-export const required = presetSchema.required.filter(prop => prop !== 'id')
